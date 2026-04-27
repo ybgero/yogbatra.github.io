e5da3dda16cdd3dc0c598a4a30728ea224b9e13b
ybgero <48390937+ybgero@users.noreply.github.com>
2026-04-27 18:27:59 +0530

    Delete everything

--- _app.jsx
+++
@@ -1,6 +0,0 @@
-import '@/styles/globals.css';
-import Head from 'next/head';
-
-export default function App({ Component, pageProps }) {
-  return <><Head><title>Yog Batra | Data Portfolio</title></Head><Component {...pageProps} /></>;
-}
